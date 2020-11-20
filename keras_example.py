fe67c702d1d1cfd7019cb4430822be14b32a3f82
James Brown <J.d.brown@student.liverpool.ac.uk>
2020-11-20 08:33:04 +0000

    Added ROC Curve, loss and accuracy for all samples
    Added two new functions that recombine split data sets for background and signal and predict binary values from the keras prediction. Used these to produce a ROC curve over the whole sample and loss and accuracy seperately for the training and test sample

--- keras_example.py
+++ keras_example.py
@@ -6,14 +6,14 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_curve
+import sklearn.metrics as metrics
 import sys
 
 doFit = True
 doPlots = True
 doScaling = True
 
-##### make plot
+##### make plot function
 def makePlot(X1,X2, tag, Nb, close, **kwargs):
     plt.clf()
     fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10.0,8.0))
@@ -26,13 +26,13 @@ def makePlot(X1,X2, tag, Nb, close, **kwargs):
         elif key=="title":
             title = value
 
-
+    #Definition of variables
     themin = min( [min(X1), min(X2)])
     themax = max( [max(X1), max(X2)])
     bins = np.linspace(themin, themax, Nb)
     width = np.zeros(len(bins)-1)
     
-    #Calculate bin centres
+    #Calculate bin centres and widths
     bincentre = np.zeros(len(bins)-1)
     for i in range(len(bins)-1):
             bincentre[i] = bins[i]+((bins[i+1]-bins[i])/2)
@@ -44,8 +44,9 @@ def makePlot(X1,X2, tag, Nb, close, **kwargs):
     #Set axis scale
     #ax.set_yscale('log')
     
-    #Implement hatches for errors
+    #Implement hatches for errors?
     #https://het.as.utexas.edu/HET/Software/Matplotlib/api/patches_api.html#matplotlib.patches.Patch.set_hatch
+    
     
     #Background plot
     plt.hist(X1, bins=bins, label=['Background'],density=True)
@@ -54,6 +55,7 @@ def makePlot(X1,X2, tag, Nb, close, **kwargs):
     back_err = np.sqrt(n_back_count)/(np.sum(n_back_count)*width)
     ax.errorbar(bincentre-err_offset, n_back_pos, xerr=None, yerr=back_err, ls='none', ecolor='k', fmt = 'ko')
     
+    
     #Signal plot
     plt.hist(X2, bins=bins, label=['Signal'], histtype=u'step',density=True)
     n_sig_pos, edge2 = np.histogram(X2, bins=Nb-1, range=(themin,themax),density=True)
@@ -61,15 +63,8 @@ def makePlot(X1,X2, tag, Nb, close, **kwargs):
     sig_err = np.sqrt(n_sig_count)/(np.sum(n_sig_count)*width)
     ax.errorbar(bincentre+err_offset, n_sig_pos, xerr=None, yerr=sig_err, ls='none', ecolor='r', fmt = 'ro')
 
+    #Calculate maximum value for y
     ymax = max([(max(n_back_pos)+max(back_err)), (max(n_sig_pos)+max(sig_err))])
-
-    # if title == "Test sample":
-    #     print("n back position: ", n_back_pos)
-    #     print("n back count: ", n_back_count)
-    #     print("X1: ", X1)
-    #     print("edge1: ", edge1)
-    #     print("edge2: ", edge2)
-    #     print("bins: ", bins)
 
     plt.title(title, fontsize=40)
     plt.xlabel(xtitle, fontsize=25)
@@ -94,6 +89,7 @@ attributes = ['# of times pregnant', 'Plasma glucose concentration', 'Diastolic 
               'Triceps skin fold thickness', '2-hour serum insulin', 'BMI', 'Diabetes pedigree function',
               'Age', 'Class variable']
 
+###Plots of input variables
 if doPlots:
     feat = [str(j) for j in attributes]
     for k,f in enumerate(feat):
@@ -121,6 +117,7 @@ else:
 #print(dataset_norm)
 #sys.exit()
 
+###Normalised plots of input variables (if doScaling is true)
 if doPlots:
     feat = [str(j)+" Normal" for j in attributes]
     for k,f in enumerate(feat):
@@ -155,8 +152,9 @@ y_test = y[N_train:]
 
 print("will split test and training samples: train ", N_train,"total:", N)
 
+'''The keras model'''
+
 if doFit:
-    # define the keras model
     model = Sequential()
     model.add(Dense(12, input_dim=8, activation='relu'))
     model.add(Dense(8, activation='relu'))
@@ -189,28 +187,118 @@ else:
 #print('Accuracy: %.2f' % (accuracy*100))
 #print(' Number of entries: ', len(X))
 
+'''Function for recombining the y data of signal and background'''
+def recombine(a):
+    length = len(a)
+    y_pred = np.zeros(length)
+    bkg_count = 0
+    res_count = 0
+
+    #Recombining predicted background and signal to match the true binary array
+    for i in range(length):
+        if a[i]==0:
+            y_pred[i]=res_bkg[bkg_count]
+            bkg_count = bkg_count+1
+        elif a[i]>0:
+            y_pred[i]=res_sig[res_count]
+            res_count = res_count+1
+        else:
+            print("ROC Curve Error")
+            
+    return y_pred
+
+'''Function for predicting binary values'''
+def predict(y_pred):
+    y_pred_test = np.zeros(len(y_pred))
+
+    for i in range(len(y_pred)):
+        if y_pred[i]<0.5:
+            y_pred_test[i]=0
+        elif y_pred[i]>=0.5:
+            y_pred_test[i]=1
+            
+    return y_pred_test
+
+
+'''All sample'''
+
+# Take all data, split into background and signal
 x_bkg =  dataset_norm[  dataset_norm[:,8] == 0  ][:,0:8] 
-x_bkg_true =  dataset_norm[dataset_norm[:,8]==0]
 x_sig =  dataset_norm[  dataset_norm[:,8] > 0  ][:,0:8] 
 
-## prediction
+# Model prediction
 res_sig = model.predict(x_sig)
 res_bkg = model.predict(x_bkg)
 
-#print(res_sig)
+
 makePlot(res_bkg.flatten(),res_sig.flatten(), "nn_pred", 20, False, xtitle="NN output", title="All sample")
 
-data_train = dataset_norm[N_train:,:]
-x_bkg = data_train[  data_train[:,8] == 0  ][:,0:8] 
-x_sig = data_train[  data_train[:,8] > 0 ][:,0:8]
+### ROC Curve
+# Recombine background and signal for entire data set
+y_pred_all = recombine(y)
+
+# Calculate the ROC curve
+fpr, tpr, thresholds = metrics.roc_curve(y, y_pred_all)
+
+# Plot (FPR against TPR)
+plt.figure(figsize=(10.0,8.0))
+plt.plot(fpr, tpr, label='RF')
+plt.xlabel('False positive rate', fontsize=25)
+plt.ylabel('True positive rate', fontsize=25)
+plt.title('ROC curve', fontsize=40)
+plt.legend(loc='best', fontsize=15)
+plt.show()
+plt.savefig("ROC_Curve.png")
 
 
-## prediction
+'''Test sample'''
+
+# Take test data, split into background and signal
+data_test = dataset_norm[N_train:,:]
+x_bkg = data_test[  data_test[:,8] == 0  ][:,0:8] 
+x_sig = data_test[  data_test[:,8] > 0 ][:,0:8]
+
+
+# Model prediction
 res_sig = model.predict(x_sig)
 res_bkg = model.predict(x_bkg)
 makePlot(res_bkg.flatten(),res_sig.flatten(), "nn_pred_test", 20, False, xtitle="NN output", title="Test sample")
 
-#fpr, tpr, thresholds = roc_curve(true binary (y), res_sig)
+# Recombine background and signal for test data set
+y_pred_test = recombine(y_test)
+
+# Predict y binary
+y_pred_test_bin = predict(y_pred_test)
+
+# Calculate loss and accuracy
+test_loss = metrics.log_loss(y_test, y_pred_test)
+test_acc = metrics.accuracy_score(y_test, y_pred_test_bin) 
+
+
+'''Train sample'''
+
+# Take train data, split into background and signal
+data_train = dataset_norm[:N_train,:]
+x_bkg = data_train[  data_train[:,8] == 0  ][:,0:8] 
+x_sig = data_train[  data_train[:,8] > 0 ][:,0:8]
+
+
+# Model prediction
+res_sig = model.predict(x_sig)
+res_bkg = model.predict(x_bkg)
+
+# Recombine background and signal for test data set
+y_pred_train = recombine(y_train)
+
+# Predict y binary
+y_pred_train_bin = predict(y_pred_train)
+
+# Calculate loss and accuracy
+train_loss = metrics.log_loss(y_train, y_pred_train)
+train_acc = metrics.accuracy_score(y_train, y_pred_train_bin)
+
+
+'''Print statements'''
 
 _,acc_train = model.evaluate(X_train, y_train)
 print('Accuracy train: {:.2f}'.format(acc_train))
@@ -223,3 +311,5 @@ print('Accuracy test: {:.2f}'.format(acc_test))
 #print(y_test)
 #print(y_test[ y_test>1  ] )
 
+print('\nTrain sample loss is: {:.2f}'.format(train_loss), '\nTrain sample accuracy is: {:.2f}'.format(train_acc))
+print('\nTest sample loss is: {:.2f}'.format(test_loss), '\nTest sample accuracy is: {:.2f}'.format(test_acc))
